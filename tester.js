9ba77070e80cdc5f98e7ae248fee883427bba1f8
Ola Al Khateeb <ola@nestrom.com>
2022-02-07 15:55:39 +0200

    add some of the consumer functionality

--- tester.js
+++ tester.js
@@ -5,10 +5,12 @@ async function main() {
     let cqClient = new CrimsonQ.CrimsonQClient({url: 'crimsonq://:crimsonQ!@127.0.0.1:9001'})
     await cqClient.init()
     let Producer = new CrimsonQ.Producer(cqClient)
-    console.log(await Producer.pushToConsumer("Ola","123"))
+    console.log(await Producer.pushToTopic("/nestrom/","123"))
 
-    let Consumer = new CrimsonQ.Consumer(cqClient)
-    console.log(await Consumer.createConsumer('ywadi' , 'nestrom/dev,test/topic'))
+    let Consumer = new CrimsonQ.Consumer(cqClient, 'Ola')
+    console.log(await Consumer.setConsumerTopics( "/dev/"))
+    console.log(await Consumer.getConsumerTopics( "/dev/"))
+    // console.log(await Consumer.completeConsumerMessage('01FVA3CM50WDS50P90JG5T9CCF' ))
 }
 
 main()
