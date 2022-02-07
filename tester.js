0f0429ddba01b7f6c80ef97b739f654b7a979b8e
Ola Al Khateeb <ola@nestrom.com>
2022-02-07 14:18:06 +0200

    remove node_modules , add gitignore , create consumer

--- tester.js
+++ tester.js
@@ -5,7 +5,10 @@ async function main() {
     let cqClient = new CrimsonQ.CrimsonQClient({url: 'crimsonq://:crimsonQ!@127.0.0.1:9001'})
     await cqClient.init()
     let Producer = new CrimsonQ.Producer(cqClient)
-    console.log(await Producer.pushToConsumer("yousef","123"))
+    console.log(await Producer.pushToConsumer("Ola","123"))
+
+    let Consumer = new CrimsonQ.Consumer(cqClient)
+    console.log(await Consumer.createConsumer('ywadi' , 'nestrom/dev,test/topic'))
 }
 
 main()
