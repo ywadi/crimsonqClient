202854c59024f307edd711bf9bc37276a87f58b5
Yousef Wadi <yousef@nestrom.com>
2022-02-08 01:47:26 +0200

    Remove node-redis and add ioredis, restructure, subscribe setup for consumer

--- tester.js
+++ tester.js
@@ -2,17 +2,41 @@ let CrimsonQ = require("./lib/crimsonq");
 
 async function main() {
 
-    let cqClient = new CrimsonQ.CrimsonQClient({url: 'crimsonq://:crimsonQ!@127.0.0.1:9001'})
-    await cqClient.init()
-    let Producer = new CrimsonQ.Producer(cqClient)
-    console.log(await Producer.pushToConsumer("Ola","123"))
+    let CQ = new CrimsonQ({
+        port: 9001, // Redis port
+        host: "127.0.0.1", // Redis host
+        password: "crimsonQ!",
+    })
+    await CQ.connect()
+    let producer = await CQ.Producer()
+    let consumer = await CQ.Consumer("yousef").init(["/wadi/yousef", "/nestrom/ceo"], 2)
 
-    let Consumer = new CrimsonQ.Consumer(cqClient)
-    await Consumer.init('Ola' , '/nestrom' , 10)
-    // console.log(await Consumer.setConsumerTopics( "/dev/"))
-    // console.log(await Consumer.getConsumerTopics( "/dev/"))
-    console.log(await Consumer.pull())
+    consumer.events.on("message", function (msg) {
+        //console.log(">>>",msg)
+        msg.fail("A BIG ERROR")
+    })
+
+    // let CQ = await cqClient.init()
+    // let Producer = new CQ.Producer(cqClient)
+
+    // let Consumer = new CQ.Consumer(cqClient)
+    // await Consumer.init('yousef', "/wadi/yousef", 2)
+    // // console.log(await Consumer.setConsumerTopics( "/dev/"))
+    // // console.log(await Consumer.getConsumerTopics( "/dev/"))
+    // Producer.pushToConsumer("yousef",{today:Date.now()})
+    // console.log(await Consumer.pull())
+    // Consumer.Subscribe()
     // console.log(await Consumer.retryMessages())
 }
 
 main()
+
+
+
+/* 
+({
+  port: 9001, // Redis port
+  host: "127.0.0.1", // Redis host
+  password: "crimsonQ!",
+}
+*/
